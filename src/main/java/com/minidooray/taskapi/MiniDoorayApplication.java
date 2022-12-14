0da1ff086b299c379da5cf03156c31bef5ff8199
최규태 <kuitae0725@gmail.com>
2022-12-14 15:46:30 +0900

    Initial commit

---
+++ src/main/java/com/minidooray/taskapi/MiniDoorayApplication.java
@@ -0,0 +1,13 @@
+package com.minidooray.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoorayApplication.class, args);
+	}
+
+}
